0c72a8fde91dc858cdd87f0b4ba1730f786b02bd
dheerajjames <dheeraj.prince7@gmail.com>
2021-08-03 16:13:47 +0530

    Final Commit String Rotation

--- questions/stringRotation.js
+++ questions/stringRotation.js
@@ -1,9 +1,24 @@
 const rotateString = (str, n, direction) => {
-  // str = input string
-  // n = number of rotations
-  //
-  // input str = random ,1, right
-  // output mrando
+
+  if(typeof str === 'string') 
+ {
+  if (direction === 'left' || direction === 'right') {
+    if(direction === 'left') {
+      let left = str.substring(n, str.length) + str.substring(0, n);
+      return left;
+    }
+    if(direction === 'right') {
+      let right = str.substring(str.length-n , str.length ) + str.substring(0, str.length-n);
+      return right;
+    }
+  }
+  else{
+    throw new Error("Invalid Direction");
+    }
+  }
+else {
+   throw new Error("Invalid Input");
+ }
 };
 
 module.exports = rotateString;
